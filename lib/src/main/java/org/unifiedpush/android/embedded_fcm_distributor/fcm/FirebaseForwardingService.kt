53026751bbb4ef414af34359709ee50cb443a49a
sim <git@sgougeon.fr>
2022-06-12 09:59:10 +0200

    Handle split messages
    Receive message up to 4096 bytes

--- lib/src/main/java/org/unifiedpush/android/embedded_fcm_distributor/fcm/FirebaseForwardingService.kt
+++ lib/src/main/java/org/unifiedpush/android/embedded_fcm_distributor/fcm/FirebaseForwardingService.kt
@@ -10,10 +10,16 @@ import org.unifiedpush.android.embedded_fcm_distributor.*
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.getTokens
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.saveFCMToken
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.sendNewEndpoint
-
-private const val TAG = "FirebaseForwarding"
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class FirebaseForwardingService : FirebaseMessagingService() {
+
+    companion object {
+        private const val TAG = "FirebaseForwarding"
+        private val pendingMessages = mutableMapOf<String, ByteArray>()
+    }
+
     override fun onNewToken(FCMToken: String) {
         Log.d(TAG, "New FCM token: $FCMToken")
         saveFCMToken(baseContext, FCMToken)
@@ -24,19 +30,61 @@ class FirebaseForwardingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "New Firebase message ${remoteMessage.messageId}")
-        // The map can be used to allow applications keeping using their old gateway for FCM
-        val message = remoteMessage.data["b"]?.let {
-            Base64.decode(it, Base64.DEFAULT)
-        } ?: JSONObject(remoteMessage.data as Map<*, *>).toString().toByteArray()
         // Empty token can be used by app not using an UnifiedPush gateway.
         val token = remoteMessage.data["i"] ?: getTokens(applicationContext).last()
+        getMessage(remoteMessage.data)?.let { message ->
+            forwardMessage(token, message, remoteMessage.messageId ?: "")
+        }
+    }
+
+    private fun forwardMessage(token: String, message: ByteArray, messageId: String) {
         val intent = Intent()
         intent.action = ACTION_MESSAGE
         intent.setPackage(baseContext.packageName)
         intent.putExtra(EXTRA_MESSAGE, String(message))
         intent.putExtra(EXTRA_BYTES_MESSAGE, message)
-        intent.putExtra(EXTRA_MESSAGE_ID, remoteMessage.messageId)
+        intent.putExtra(EXTRA_MESSAGE_ID, messageId)
         intent.putExtra(EXTRA_TOKEN, token)
         baseContext.sendBroadcast(intent)
+    }
+
+    private fun getMessage(data: MutableMap<String, String>): ByteArray? {
+        var message: ByteArray? = null
+        data["b"]?.let { b64 ->
+            if (!Regex("^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$")
+                    .matches(b64)) {
+                // The map can be used to allow applications keeping using their old gateway for FCM
+                return JSONObject(data as Map<*, *>).toString().toByteArray()
+            }
+            data["m"]?.let { mId ->
+                data["s"]?.let { splitId ->
+                    if (pendingMessages.containsKey(mId)) {
+                        Log.d(TAG, "Found pending message")
+                        when (splitId) {
+                            "1" -> {
+                                message = Base64.decode(b64, Base64.DEFAULT) +
+                                        pendingMessages[mId]!!
+                            }
+                            "2" -> {
+                                message = pendingMessages[mId]!! +
+                                        Base64.decode(b64, Base64.DEFAULT)
+                            }
+                        }
+                        pendingMessages.remove(mId)
+                    } else {
+                        pendingMessages[mId] = Base64.decode(b64, Base64.DEFAULT)
+                        Timer().schedule(3000) {
+                            pendingMessages.remove(mId)
+                        }
+                    }
+                }
+            } ?: run {
+                return Base64.decode(b64, Base64.DEFAULT)
+            }
+        } ?: run {
+            // The map can be used to allow applications keeping using their old gateway for FCM
+            return JSONObject(data as Map<*, *>).toString().toByteArray()
+        }
+        return message
     }
 }
