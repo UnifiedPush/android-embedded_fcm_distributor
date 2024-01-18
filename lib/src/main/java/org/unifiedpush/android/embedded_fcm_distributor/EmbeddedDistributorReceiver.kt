e5493ef6146b8fe8510177ec36ab64ba4b1b77a5
S1m <git@sgougeon.fr>
2024-01-18 23:04:56 +0100

    Send registration fail without PlayServices

--- lib/src/main/java/org/unifiedpush/android/embedded_fcm_distributor/EmbeddedDistributorReceiver.kt
+++ lib/src/main/java/org/unifiedpush/android/embedded_fcm_distributor/EmbeddedDistributorReceiver.kt
@@ -5,10 +5,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.messaging.FirebaseMessaging
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.removeToken
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.saveToken
 import org.unifiedpush.android.embedded_fcm_distributor.Utils.sendNewEndpoint
+import org.unifiedpush.android.embedded_fcm_distributor.Utils.sendRegistrationFailed
 
 private const val TAG = "UP-Embedded_distributor"
 
@@ -25,6 +28,22 @@ open class EmbeddedDistributorReceiver : BroadcastReceiver() {
                 Log.d(TAG, "Registering to the embedded distributor")
                 saveGetEndpoint(context)
                 saveToken(context, token)
+                when (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context)) {
+                    ConnectionResult.SUCCESS -> Log.d(TAG, "PlayServices available")
+                    ConnectionResult.SERVICE_MISSING,
+                    ConnectionResult.SERVICE_DISABLED,
+                    ConnectionResult.SERVICE_INVALID -> {
+                        Log.w(TAG, "PlayServices Missing, disabled or invalid. Sending registration failed")
+                        sendRegistrationFailed(context, token, "PlayServices not available")
+                        return
+                    }
+                    ConnectionResult.SERVICE_UPDATING,
+                    ConnectionResult.SERVICE_VERSION_UPDATE_REQUIRED -> {
+                        Log.w(TAG, "PlayServices updating or require an update.")
+                        sendRegistrationFailed(context, token, "PlayServices temporarily not available")
+                        return
+                    }
+                }
                 FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
                     if (task.isSuccessful) {
                         val fcmToken = task.result
@@ -33,6 +52,7 @@ open class EmbeddedDistributorReceiver : BroadcastReceiver() {
                     } else {
                         Log.w(TAG, "FCMToken registration failed: " +
                             "${task.exception?.localizedMessage}")
+                        sendRegistrationFailed(context, token, "FCM (PlayServices) token not received")
                     }
                 }
             }
